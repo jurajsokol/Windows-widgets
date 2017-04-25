9e59f077b49253af5efbcb199001deb8475baad7
Juraj Sokol <jurajsokol.s@gmail.com>
2017-04-25 20:44:35 +0200

    Add project files.

---
+++ Windows 10 app/BlurFeature/NativeBlurBackground.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+
+namespace Windows_10_app.BlurFeature
+{
+    class NativeBlurBackground
+    {
+        [DllImport("user32.dll")]
+        internal static extern int SetWindowCompositionAttribute(IntPtr hwnd, ref WindowCompositionAttributeData data);
+
+        static internal void EnableBlur(MainWindow m)
+        {
+            var windowHelper = new WindowInteropHelper(m);
+
+            var accent = new AccentPolicy();
+            accent.AccentState = AccentState.ACCENT_ENABLE_BLURBEHIND;
+
+            var accentStructSize = Marshal.SizeOf(accent);
+
+            var accentPtr = Marshal.AllocHGlobal(accentStructSize);
+            Marshal.StructureToPtr(accent, accentPtr, false);
+
+            var data = new WindowCompositionAttributeData();
+            data.Attribute = WindowCompositionAttribute.WCA_ACCENT_POLICY;
+            data.SizeOfData = accentStructSize;
+            data.Data = accentPtr;
+
+            SetWindowCompositionAttribute(windowHelper.Handle, ref data);
+
+            Marshal.FreeHGlobal(accentPtr);
+        }
+    }
+}
---
+++ Windows 10 app/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+
+namespace Windows_10_app
+{
+    public partial class MainWindow : Window
+    {
+        int PADDING = 5;
+
+        DateTime localDate = DateTime.Now;        
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.ShowInTaskbar = false;
+            this.Left = SystemParameters.PrimaryScreenWidth - this.Width - PADDING;
+            System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer(new TimeSpan(0, 0, 1), System.Windows.Threading.DispatcherPriority.Normal, delegate
+            {
+                this.digitalClock.Text = DateTime.Now.ToString("HH:mm");
+            }, this.Dispatcher);
+            System.Windows.Threading.DispatcherTimer date = new System.Windows.Threading.DispatcherTimer(new TimeSpan(0, 0, 1), System.Windows.Threading.DispatcherPriority.Normal, delegate
+            {
+                this.dateClock.Text = DateTime.Now.ToString("dd. MMMMM");
+            }, this.Dispatcher);
+
+        }
+
+        private void CloseApp(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Windows_10_app.BlurFeature.NativeBlurBackground.EnableBlur(this);
+        }
+
+        
+
+    }
+}
